021a3a304ca53fd588b2cae0023ddb68e8f41e5e
BrandooDi <nikita1231w@gmail.com>
2022-12-04 22:37:39 +0300

    done

---
+++ Project/ind1.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def get_plat():
+    """
+    Запросить данные о платежах.
+    """
+    raspol = input("Расчётный счёт платильщика: ")
+    raspl = input("Расчётный счет получателя: ")
+    sum = input("Перечисляемая сумма в руб: ")
+
+    # Создать словарь.
+    return {
+        "raspol": raspol,
+        "raspl": raspl,
+        "sum": sum,
+    }
+
+
+def display_plats(staff):
+    """
+    Отобразить список платежей.
+    """
+    # Проверить, что список платежей не пуст.
+    if staff:
+        # Заголовок таблицы.
+        line = "+-{}-+-{}-+-{}-+-{}-+".format("-" * 4, "-" * 30, "-" * 35, "-" * 45)
+        print(line)
+        print(
+            "| {:^4} | {:^30} | {:^35} | {:^45} |".format(
+                "No",
+                "Расчётный счёт платильщика",
+                "Расчётный счет получателя",
+                "Перечисляемая сумма в руб",
+            )
+        )
+        print(line)
+
+        # Вывести данные о всех платежах.
+        for idx, plat in enumerate(staff, 1):
+            print(
+                "| {:>4} | {:<30} | {:<35} | {:>45} |".format(
+                    idx,
+                    plat.get("raspol", ""),
+                    plat.get("raspl", ""),
+                    plat.get("sum", ""),
+                )
+            )
+
+        print(line)
+
+    else:
+        print("Список платежей пуст")
+
+
+def select_plats(staff, jet):
+    """
+    Выбрать сумму с данным типом.
+    """
+    # Сформировать список  платежей.
+    result = [plat for plat in staff if jet == plat.get("sum", "")]
+
+    # Возвратить список выбранных платежей.
+    return result
+
+
+def main():
+    """
+    Главная функция программы.
+    """
+    # Список платежей.
+    plats = []
+
+    # Организовать бесконечный цикл запроса команд.
+    while True:
+        # Запросить команду из терминала.
+        command = input(">>> ").lower()
+
+        # Выполнить действие в соответствие с командой.
+        if command == "exit":
+            break
+
+        elif command == "add":
+            # Запросить данные о платеже.
+            plat = get_plat()
+
+            # Добавить словарь в список.
+            plats.append(plat)
+            # Отсортировать список в случае необходимости.
+            if len(plats) > 1:
+                plats.sort(key=lambda item: item.get("destination", ""))
+
+        elif command == "list":
+            # Отобразить все платежи.
+            display_plats(plats)
+
+        elif command.startswith("select "):
+            # Разбить команду на части для выделения пункта назначения.
+            part = command.split(" ", maxsplit=1)
+            com = part[1]
+
+            # Выбрать платежи заданного типа
+            selected = select_plats(plats, com)
+            # Отобразить выбранные платежи
+            display_plats(selected)
+
+        elif command == "help":
+            # Вывести справку о работе с программой.
+            print("Список команд:\n")
+            print("add - добавить самолет;")
+            print("list - вывести список самолетов;")
+            print("select <тип> - запросить самолеты заданного типа;")
+            print("help - отобразить справку;")
+            print("exit - завершить работу с программой.")
+        else:
+            print(f"Неизвестная команда {command}", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Project/zd2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+
+
+def cylinder():
+    """
+    Вычисление полной площади или площади боковой поверхности цилиндра
+    """
+    way = int(
+        input(
+            "Какую площадь вы хотите вычислить?\n"
+            "Боковой поверхности(1)\n"
+            "Полную площадь(2)\n"
+            "> "
+        )
+    )
+
+    r = float(input("Введите радиус: "))
+    h = float(input("Введите высоту: "))
+    s = 2 * math.pi * r * h
+
+    if way == 1:
+        print(f"Боковая площадь s = {s}")
+
+    elif way == 2:
+        s += circle(r) * 2
+        print(f"Полная площадь s = {s}")
+
+    else:
+        print("Ошибка")
+
+
+def circle(r):
+    """
+    Вычисление площади основания
+    """
+    return math.pi * pow(r, 2)
+
+
+if __name__ == "__main__":
+    cylinder()
